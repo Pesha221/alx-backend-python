1e75ef164471c7081d217aef171647913c58c6b0
Pesha221 <onyangojames221@gmail.com>
2025-11-17 00:56:19 +0300

    Implement serializers

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 "api/"
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls'))
+    path("api/", include('chats.urls'))
     
 ]
