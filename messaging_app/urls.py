cbd890ca259ad25a3ba6f61a9065fb2bbecbd165
Pesha221 <onyangojames221@gmail.com>
2025-11-17 01:32:44 +0300

    Implement configuration of  URLS for the conversations and messages

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-"api-auth"
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),  # All app API routes
-    path('api-auth/', include('rest_framework.urls')),  # ⚡ Required for DRF auth
+    path('api/', include('chats.urls')),      # All your app API endpoints
+    path('api-auth/', include('rest_framework.urls')),  
 ]
 
