88d82c5a6a8b668ed623e053990c13970093948b
Pesha221 <onyangojames221@gmail.com>
2025-11-17 00:39:54 +0300

    Implement serializers

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -1,11 +1,7 @@
+from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from chats.views import ConversationViewSet, MessageViewSet
-
-router = DefaultRouter()
-router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include('chats.urls')),  # This line adds API endpoints
 ]
