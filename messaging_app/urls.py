4f75d08b6bdd832906ade07aec7ed279860672eb
Pesha221 <onyangojames221@gmail.com>
2025-11-17 01:29:43 +0300

    Implement configuration of  URLS for the conversations and messages

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+"api-auth"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),  # All app API routes
