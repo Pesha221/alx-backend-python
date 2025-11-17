084131d7ac5669446f97fa2cccc6eee11e536511
Pesha221 <onyangojames221@gmail.com>
2025-11-18 00:21:00 +0300

    Recreate clean url routing files

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_nested.routers import NestedDefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter # type: ignore
 from .views import ConversationViewSet, MessageViewSet
 
 "routers.DefaultRouter()"
