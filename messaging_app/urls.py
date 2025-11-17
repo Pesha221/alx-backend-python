11f5bb6413dcfaa08e3a44105e9f5b363b3c9bd3
Pesha221 <onyangojames221@gmail.com>
2025-11-17 09:47:01 +0300

    Recreate clean url routing files

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
+["api-auth"]
+["api/"]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('api-auth/', include('rest_framework.urls')),
     path("api/", include('chats.urls')),
 ]
-"api/"
+
 
