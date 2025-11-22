690ce150c5f9290cbc7fdd3f3561f1764b5d21f9
Pesha221 <onyangojames221@gmail.com>
2025-11-22 13:14:12 +0300

    Add changes to client.py file

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
-"""Github org client
+"""A github org client
 """
 
 from typing import List, Dict
-from utils import get_json, access_nested_map, memoize # type: ignore
+
+from utils import (
+    get_json,
+    access_nested_map,
+    memoize,
+)
 
 
 class GithubOrgClient:
-    """Github Org Client"""
-
+    """A Github org client
+    """
     ORG_URL = "https://api.github.com/orgs/{org}"
 
     def __init__(self, org_name: str) -> None:
@@ -17,29 +22,32 @@ class GithubOrgClient:
 
     @memoize
     def org(self) -> Dict:
-        """Return org data"""
+        """Returns the org JSON payload"""
         return get_json(self.ORG_URL.format(org=self._org_name))
 
     @property
     def _public_repos_url(self) -> str:
-        """Return repos_url from org"""
-        return self.org.get("repos_url")
+        """Returns the URL to the public repos"""
+        return self.org["repos_url"]
+
+    @memoize
+    def repos_payload(self) -> Dict:
+        """Returns the repos payload"""
+        return get_json(self._public_repos_url)
 
     def public_repos(self, license: str = None) -> List[str]:
-        """List of public repos, optionally filtered by license"""
-        repos = get_json(self._public_repos_url)
+        """List public repos optionally filtered by license"""
+        repos = self.repos_payload
 
-        repo_names = [
+        return [
             repo["name"]
             for repo in repos
             if license is None or self.has_license(repo, license)
         ]
 
-        return repo_names
-
     @staticmethod
     def has_license(repo: Dict, license_key: str) -> bool:
-        """Check whether repo has a given license"""
+        """Check if repo has the given license"""
         try:
             return access_nested_map(repo, ("license", "key")) == license_key
         except KeyError:
